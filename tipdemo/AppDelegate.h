0c8d71d1baf49904c1e4d60fa388091cdb34ba1a
Hirak Chatterjee <hirakchatterjee@gmail.com>
2014-06-05 17:12:09 -0700

    Initial Commit

---
+++ tipdemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  tipdemo
+//
+//  Created by Hirak Chatterjee on 6/5/14.
+//  Copyright (c) 2014 Hirak Chatterjee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
